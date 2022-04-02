65080b3ee3f2033ff1c56e2e812cdebffcf58824
jsan <ahsan.wtc@gmail.com>
2022-04-02 13:43:36 +0500

    tested transfer

--- test/ERC20.test.js
+++ test/ERC20.test.js
@@ -1,3 +1,5 @@
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+
 const ERC20 = artifacts.require('ERC20');
 
 /*
@@ -8,9 +10,10 @@ const ERC20 = artifacts.require('ERC20');
 contract('ERC20', accounts => {
   let token = null;
   const name = 'My Token', symbol = 'MTK', decimals = 18, totalSupply = 100000;
+  const [owner, user, spender] = accounts;
 
-  before('before tests start', async () => {
-    token = await ERC20.deployed();
+  beforeEach('before tests start', async () => {
+    token = await ERC20.new(name, symbol, decimals, totalSupply);
   });
 
   it('should be deployed correctly', async () => {
@@ -18,6 +21,26 @@ contract('ERC20', accounts => {
     assert(await token.symbol() === symbol, 'symbol not correct');
     assert((await token.decimals()).toNumber() === decimals, 'decimals not correct');
     assert((await token.totalSupply()).toNumber() === totalSupply, 'totalSupply not correct');
+  });
+
+  it('should transfer tokens', async () => {
+    const tokenToTransfer = 50;
+    const balanceBefore = await token.balanceOf(user);
+    const receipt = await token.transfer(user, tokenToTransfer, { from: owner });
+    const balanceAfter = await token.balanceOf(user);
+
+    const diff = balanceAfter.sub(balanceBefore).toNumber();
+    assert(diff === tokenToTransfer);
+    assert((await token.balanceOf(owner)).toNumber() === totalSupply - tokenToTransfer);
+    expectEvent(receipt, 'Transfer', { from: owner, to: user, value: web3.utils.toBN(tokenToTransfer) });
+  });
+
+  it('should NOT transfer tokens if balance is not enough', async () => {
+    const tokenToTransfer = totalSupply + 1;
+    await expectRevert(
+      token.transfer(user, tokenToTransfer, { from: owner }),
+      'not enough balance'
+    );
   });
 
 });
