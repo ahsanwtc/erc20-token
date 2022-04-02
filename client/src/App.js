7f01414ccd22437497acd883a8886a8c33cc7b51
jsan <ahsan.wtc@gmail.com>
2022-04-02 14:18:44 +0500

    scaffolding frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ERC 20
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
